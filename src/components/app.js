03f5babc43f5410df8060b83dae94feadb088866
Junaid Farooq <junaid@evercam.io>
2016-10-12 18:06:00 +0500

    Added todos const

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
+const todos = [
+  {
+    task: "Move on from here",
+    isCompleted: false
+  },
+  {
+    task: "Move here from on",
+    isCompleted: true
+  }
+];
+
 export default class App extends React.Component {
   render() {
     return (
